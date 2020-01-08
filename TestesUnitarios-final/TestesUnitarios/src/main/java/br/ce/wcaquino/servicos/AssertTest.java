58ec5442dc29a4b36e359a464ac038e973ffa178
Tayna Rodrigues <taynajesus@gmail.com>
2020-01-08 11:44:32 -0400

    organizando os arquivos de testes e utilizando Assertivas

---
+++ TestesUnitarios-final/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/AssertTest.java
@@ -0,0 +1,50 @@
+package br.ce.wcaquino.servicos;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import org.junit.Assert;
+import org.junit.Assert.*;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Usuario;
+
+
+
+public class AssertTest {
+	
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+		Assert.assertFalse(false);
+		
+		Assert.assertEquals("Erro de comparação", 1, 2);
+		Assert.assertEquals(0.512, 0.512, 0.001);
+		Assert.assertEquals(Math.PI, 3.14, 0.01);
+		
+		int i = 5;
+		Integer i2 = 5;
+		Assert.assertEquals(Integer.valueOf(i), i2);
+		Assert.assertEquals(i,  i2.intValue());
+		
+		
+		Assert.assertEquals("bola", "bola");
+		Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
+		
+		Assert.assertNotEquals("bola", "casa");
+		Assert.assertTrue("bola".startsWith("bo"));
+		
+		Usuario u1 = new Usuario("Usuario 1");
+		Usuario u2 = new Usuario("Usuario 1");
+		Usuario u3 = null;
+		
+		Assert.assertEquals(u1, u2);
+		
+		Assert.assertSame(u2, u2);
+		Assert.assertNotSame(u1, u2);
+		
+		Assert.assertNull(u3);
+		Assert.assertNotNull(u2);
+	}
+
+}
--- TestesUnitarios-final/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ TestesUnitarios-final/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -7,7 +7,6 @@ import java.util.Date;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -27,20 +26,5 @@ public class LocacaoService {
 		//TODO adicionar método para salvar
 		
 		return locacao;
-	}
-	
-	public static void main(String[] args) {
-		//cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 2, 5.0);
-		
-		//acao
-		Locacao locacao = service.alugarFilme(usuario, filme);
-		
-		//verificacao
-		System.out.println(locacao.getValor() == 5.0);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
 }
